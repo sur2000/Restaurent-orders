ac9efc50f8fea7327a4fa59a7cb5e3a3e0c27f3f
sur shri <surshri2001@gmail.com>
2022-05-31 13:05:26 +0530

    menucontainer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
 
 import './App.css';
-import Header from './Components/Header';
+import Header from './Components/Header'; 
+import MenuContainer from "./Components/MenuContainer";
+import {
+  AccountBalanceWalletRounded,
+  Chat,
+  Favorite,
+  HomeRounded,
+  Settings,
+  SummarizeRounded,
+} from "@mui/icons-material"; 
 
-function App() {
+import { useEffect, useState } from "react";
+
+
+
+function App() {   
+
+  useEffect(() => {
+    const menuLi = document.querySelectorAll("#menu li");
+
+    function setMenuActive() {
+      menuLi.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }  
+
+    menuLi.forEach((n) => n.addEventListener("click", setMenuActive));
+
+  }, []);
+
+
+
+
+
+
   return (
     <div className="App">
       <Header />
+
+     <main>
+      
+      
+
+     </main> 
+
+     <div className="bottomMenu">
+        <ul id="menu">
+          {/* prettier-ignore */}
+          <MenuContainer link = {'#'} icon = {<HomeRounded />}  isHome/>
+          {/* prettier-ignore */}
+          <MenuContainer link = {'#'} icon = {<Chat />}  />
+          {/* prettier-ignore */}
+          <MenuContainer link = {'#'} icon = {<AccountBalanceWalletRounded />}  />
+          {/* prettier-ignore */}
+          <MenuContainer link = {'#'} icon = {<Favorite />} />
+          {/* prettier-ignore */}
+          <MenuContainer link = {'#'} icon = {<SummarizeRounded />}  />
+          {/* prettier-ignore */}
+          <MenuContainer link = {'#'} icon = {<Settings />}  />
+          <div className="indicator"></div>
+        </ul>
+      </div>
+
     </div>
   );
 }
